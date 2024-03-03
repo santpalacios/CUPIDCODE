3489f772f78a09a804fc1901cc4f97bf4655a5bc
cesararanda <ceesaruribea@gmail.com>
2024-03-02 22:53:46 -0600

    segunda actualizacion
    Signed-off-by: cesararanda <ceesaruribea@gmail.com>

--- MODAL-main/js/registro.js
+++ MODAL-main/js/registro.js
@@ -1,10 +1,39 @@
-const container = document.querySelector(".container");
-const btnSignIn = document.getElementById("btn-sign-in");
-const btnSignUp = document.getElementById("btn-sign-up");
+const inputs = document.querySelectorAll(".input-field");
+const toggle_btn = document.querySelectorAll(".toggle");
+const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
 
-btnSignIn.addEventListener("click",()=>{
-   container.classList.remove("toggle");
+inputs.forEach((inp) => {
+  inp.addEventListener("focus", () => {
+    inp.classList.add("active");
+  });
+  inp.addEventListener("blur", () => {
+    if (inp.value != "") return;
+    inp.classList.remove("active");
+  });
 });
-btnSignUp.addEventListener("click",()=>{
-   container.classList.add("toggle");
+
+toggle_btn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    main.classList.toggle("sign-up-mode");
+  });
+});
+
+function moveSlider() {
+  let index = this.dataset.value;
+
+  let currentImage = document.querySelector(`.img-${index}`);
+  images.forEach((img) => img.classList.remove("show"));
+  currentImage.classList.add("show");
+
+  const textSlider = document.querySelector(".text-group");
+  textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
+
+  bullets.forEach((bull) => bull.classList.remove("active"));
+  this.classList.add("active");
+}
+
+bullets.forEach((bullet) => {
+  bullet.addEventListener("click", moveSlider);
 });
