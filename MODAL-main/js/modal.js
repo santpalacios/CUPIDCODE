34322dcf34162ac4cfbae5a09e38039fc30c53d9
cesararanda <ceesaruribea@gmail.com>
2024-03-03 18:47:38 -0600

    tercer actualizacion

--- MODAL-main/js/modal.js
+++ MODAL-main/js/modal.js
@@ -11,3 +11,22 @@ document.addEventListener('DOMContentLoaded', function() {
         modal_container.classList.remove('show');
     });
 });
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const open = document.getElementById('open');
+//     const modal_container = document.getElementById('modal_container');
+//     const close = document.getElementById('close');
+
+//     open.addEventListener('click', () => {
+//         modal_container.classList.add('show');
+//         // Espera 5 segundos y redirige a otra página
+//         setTimeout(() => {
+//             window.location.href = 'index.html';
+//         },2000);
+//     });
+
+// });
+
+
